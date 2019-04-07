3a5569c9b5acf30c50e5273f586eac9973b7a1de
xjy <Xin18792900229@163.com>
2019-04-07 14:31:54 +0800

    Daily record

--- src/offer/april/day04/InversePairs.java
+++ src/offer/april/day04/InversePairs.java
@@ -3,7 +3,7 @@ package offer.april.day04;
 import java.util.Arrays;
 
 /**
- * ClassName InversePairs<br>
+ * ClassName inversepairs<br>
  * Function <br>
  * <p>在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数
  * 组,求出这个数组中的逆序对的总数P。并将P对1000000007取模的结果输出。 即输出P%1000000007</p>
@@ -12,7 +12,7 @@ import java.util.Arrays;
  * @version 1.0.0
  */
 public class InversePairs {
-    public void InversePairs(int[] array) {
+    public void inversepairs(int[] array) {
         if (array.length < 2) {
             return;
         }
